b2dcb6838b33611b794b61423e183816f185edbb
levg34 <levg34@users.noreply.github.com>
2023-01-16 18:19:30 +0100

    init ws server

---
+++ server.ts
@@ -0,0 +1,15 @@
+import { WebSocketServer } from 'ws'
+
+const wss = new WebSocketServer({ port: 8080 })
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', function message(data) {
+        console.log('received: %s', data)
+
+        wss.clients.forEach(client => {
+            client.send(data.toString())
+        })
+    })
+
+    ws.send('Hello there.')
+})
