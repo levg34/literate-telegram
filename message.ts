6d153ddc4ab76f16f3082aaf9766f97b93a4b517
levg34 <levg34@users.noreply.github.com>
2023-01-17 18:13:57 +0100

    assign color to user

--- message.ts
+++ message.ts
@@ -2,6 +2,7 @@ export interface IMessage {
     message: string
     sender?: string
     time?: string
+    color?: string
 }
 
 export interface IMessageJSON {
@@ -14,12 +15,14 @@ export class Message implements Required<IMessage> {
     message: string
     sender: string
     time: string
+    color: string
     static ME = 'Server'
 
     constructor(message: IMessage) {
         this.message = message.message
         this.sender = message.sender ?? Message.ME
         this.time = message.time ?? new Date().toISOString()
+        this.color = message.color ?? '#D3D3D3'
     }
 
     toJSON(): Required<IMessageJSON> {
--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 import WebSocket, { WebSocketServer } from 'ws'
 import { Message } from './message'
 import { randomUUID } from 'crypto'
+import colors from './colors.json'
 
 const wss = new WebSocketServer({ port: 8080 })
 
@@ -10,14 +11,34 @@ const stringifyMessage = (message: Message): string => {
 
 interface WSId extends WebSocket.WebSocket  {
     id?: string
+    color?: string
 }
 
+type User = {
+    id: string
+    color: string
+    ws: WebSocket.WebSocket
+}
+
+const users: User[] = []
+
 wss.on('connection', function connection(ws: WSId) {
-    ws.id = randomUUID()
+    const user: User = {
+        id: randomUUID(),
+        color: colors[users.length%(colors.length)], //getColor(users.length),
+        ws
+    }
+
+    users.push(user)
+
+    ws.id = user.id
+    ws.color = user.color
+
     ws.on('message', function message(data) {
         console.log('received: %s', data)
         const received = new Message(JSON.parse(data.toString()))
         received.sender = ws.id || 'Unknown'
+        received.color = users.find(u => u.id === ws.id)?.color || '#D3D3D3'
 
         wss.clients.forEach((client: WSId) => {
             const clientId = client.id
